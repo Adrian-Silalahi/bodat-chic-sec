7ed0f4a56b7988982f52ff932aa6492290eb0406
Adrianus Silalahi <211112269@students.mikroskil.ac.id>
2026-04-23 09:20:14 +0700

    replace bcrypt with bcryptjs

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,46 +1,46 @@
-import prisma from '@/src/libs/prismadb'
-import bcrypt from 'bcrypt'
-import { NextResponse } from 'next/server'
+import prisma from "@/src/libs/prismadb";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
 
-export async function POST (request: Request): Promise<NextResponse> {
-  const body = await request.json()
-  const { name, email, password } = body
+export async function POST(request: Request): Promise<NextResponse> {
+  const body = await request.json();
+  const { name, email, password } = body;
 
   const checkName = await prisma.user.findUnique({
     where: {
-      name
-    }
-  })
+      name,
+    },
+  });
 
   if (checkName) {
     return NextResponse.json(
-      { message: 'Nama ini sudah terdaftar' },
-      { status: 409 }
-    )
+      { message: "Nama ini sudah terdaftar" },
+      { status: 409 },
+    );
   }
 
   const checkEmail = await prisma.user.findUnique({
     where: {
-      email
-    }
-  })
+      email,
+    },
+  });
 
   if (checkEmail) {
     return NextResponse.json(
-      { message: 'Email ini sudah terdaftar' },
-      { status: 409 }
-    )
+      { message: "Email ini sudah terdaftar" },
+      { status: 409 },
+    );
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10)
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await prisma.user.create({
     data: {
       name,
       email,
-      hashedPassword
-    }
-  })
+      hashedPassword,
+    },
+  });
 
-  return NextResponse.json(user)
+  return NextResponse.json(user);
 }
