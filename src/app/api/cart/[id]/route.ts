68801560ea0e71928e132794508af8977174ae97
Adrianus Silalahi <211112269@students.mikroskil.ac.id>
2026-04-23 09:13:15 +0700

    fix cart api build error

--- src/app/api/cart/[id]/route.ts
+++ src/app/api/cart/[id]/route.ts
@@ -1,24 +1,28 @@
-import { getCurrentUser } from '@/src/actions/getCurrentUser'
-import prisma from '@/src/libs/prismadb'
-import { NextResponse } from 'next/server'
+export const dynamic = "force-dynamic";
 
-export async function DELETE (request: Request, { params }: { params: { id: string } }): Promise<NextResponse> {
-  const currentUser = await getCurrentUser()
-  
+import { getCurrentUser } from "@/src/actions/getCurrentUser";
+import prisma from "@/src/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+): Promise<NextResponse> {
+  const currentUser = await getCurrentUser();
+
   if (!currentUser) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-  try{
+  try {
     const cartProduct = await prisma.cartProduct.delete({
       where: {
         id: params.id,
         userId: currentUser.id,
-      }
-    })
-  
-    return NextResponse.json(cartProduct)
-  }
-  catch(error: any){
-    return NextResponse.json({ error }, { status: 500 })
+      },
+    });
+
+    return NextResponse.json(cartProduct);
+  } catch (error: any) {
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
